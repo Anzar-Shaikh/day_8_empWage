f82ec436908467dc3d9c09c32b4228c800eab399
Anzar Shaikh <anzarshaikh373@gmail.com>
2021-06-28 19:39:10 +0530

    Daily wage of employe is calculated.

--- EmpWage.java
+++ EmpWage.java
@@ -6,9 +6,11 @@ class utilityEmpWage{
     Random rand = new Random();
     public int isPresent = 1;
     public int isAbsent = 2;
+    public int wagePerHr = 20;
+    public int fullDayHr = 8;
+    int check = rand.nextInt(2)+1;
 
     public void present() {
-        int check = rand.nextInt(2)+1;
         if (check == isAbsent) {
             System.out.println("Employee is absent.");
         }
@@ -16,10 +18,20 @@ class utilityEmpWage{
             System.out.println("Employee is present.");
         }
     }
+    public void dailyWage(){
+        if (check == isPresent) {
+            int dailyWage = wagePerHr*fullDayHr;
+            System.out.println("Wage of the day of employee is : " + dailyWage);
+        }
+    }
 }
+
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation problem.");
+        utilityEmpWage empWage = new utilityEmpWage();
+        empWage.present();
+        empWage.dailyWage();
 
     }
 }
